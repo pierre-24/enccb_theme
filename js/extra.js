fa0cb83468d772b1032377211161a03589b3233e
Pierre Beaujean <pierreb24@gmail.com>
2022-03-10 16:53:44 +0100

    add anchor to h2

--- js/extra.js
+++ js/extra.js
@@ -1,3 +1,18 @@
+// add title to each image, based on the alt desc
 document.querySelectorAll('article img').forEach($e => {
-  $e.title = $e.alt; // add title to each image, based on the alt desc
+  $e.title = $e.alt;
+});
+
+// add an anchor to each title
+let title_id = 1;
+document.querySelectorAll('article h2').forEach($e => {
+  let $link = document.createElement('a');
+  let _id = `title-${title_id}`;
+  $link.innerText = '#';
+  $link.href= '#' + _id;
+  $link.classList.add('anchor')
+  $e.id = _id;
+
+  $e.appendChild($link);
+  title_id += 1;
 });
