16bef1d3749733362ff91dbb1b26de366f403870
Pierre Beaujean <pierreb24@gmail.com>
2022-04-27 14:28:26 +0200

    slugify the title (quite agressively)

--- js/extra.js
+++ js/extra.js
@@ -3,16 +3,28 @@ document.querySelectorAll('article img').forEach($e => {
   $e.title = $e.alt;
 });
 
-// add an anchor to each title
-let title_id = 1;
+// add an anchor to each title, using the existing id or a new one if it does not exist
 document.querySelectorAll('article h2').forEach($e => {
   let $link = document.createElement('a');
-  let _id = `title-${title_id}`;
+  let _id;
+
+  if(!$e.id) {
+    // (aggressively) slugify the text
+    let txt = $e.innerHTML
+      .replace(/<(.|\n)*?>/g, '')
+      .replace(/[\s_.,?!]/g, ' ').trim()
+      .replace(/\s+/g, '-')
+      .replace(/[^0-9a-z\-]/gi, ''); // only [0-9a-z\-] is kept
+    _id = txt.toLowerCase();
+    // set the result as id
+    $e.id = _id;
+  } else {
+    _id = $e.id; // use existing id
+  }
+
   $link.innerText = '#';
   $link.href= '#' + _id;
   $link.classList.add('anchor')
-  $e.id = _id;
 
   $e.appendChild($link);
-  title_id += 1;
 });
